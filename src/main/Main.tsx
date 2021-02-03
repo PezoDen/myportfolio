bfce5d4297d6a956b92145e83460fa14d5c46575
PezoDen <pezo1981@gmail.com>
2021-02-03 18:20:07 +0300

    .pack

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -5,7 +5,7 @@ import styleContainer from "../common/styles/Container.module.css"
 function Main() {
   return (
     <div className={style.mainBlock}>
-      <div className={`{styleContainer.container} {style.mainContainer}`}>
+      <div className={`${styleContainer.container} ${style.mainContainer}`}>
         <div className={style.text}>
           <span>Hi There</span>
           <h1>I am Jeffrey Aaron</h1>
